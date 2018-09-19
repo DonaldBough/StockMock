92bcf48f86af001655cbca7519349275da8ed464
Harsh Parakh <hparakh@purdue.edu>
2018-09-19 17:34:25 -0400

    Fetching number of shares purchased for each company on StockMock

--- code/app/statistics/statistics.js
+++ code/app/statistics/statistics.js
@@ -23,8 +23,32 @@ query.once("value")
     snapshot.forEach(function() {
     count++
   });
-console.log(count);
+//console.log(count);
 });
 
+//Fetch number of shares purchased for each company on StockMock
+var map = new Map();
+var query = firebase.database().ref("user").orderByKey();
+query.once("value").then(function(snapshot) {
+    snapshot.forEach(function(users) {
+      var uid = users.key;
+      var path = "/user/"+uid+"/stocks/";
+      var query = firebase.database().ref(path).orderByKey();
+      query.once("value").then(function(snapshot) {
+          snapshot.forEach(function(stock) {
+            var symbol = String(stock.key).toUpperCase();
+            var shares = parseInt(stock.val(),10);
+            if(!map.has(symbol)){
+              map.set(symbol, shares);
+            }
+            else {
+              map.set(symbol, map.get(symbol)+shares);
+            }
+          });
+      });
+    });
+});
+
+console.log(map);
 
 });
