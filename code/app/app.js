026d19ef5300183610a1854c7f7820c16dec0ef1
Harsh Parakh <hparakh@purdue.edu>
2018-09-19 13:58:37 -0400

    Created Statistics Page on the Navbar

--- code/app/app.js
+++ code/app/app.js
@@ -12,7 +12,8 @@ var app = angular.module('myApp', [
   'myApp.settings',
   'myApp.login',
   'myApp.change-password',
-  'myApp.viewSearch'
+  'myApp.viewSearch',
+  'myApp.statistics'
 ]);
 
 app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
---
+++ code/app/statistics/statistics.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('myApp.statistics', ['ngRoute', 'ngCookies']) //'ngSanitize'
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/statistics', {
+    templateUrl: 'statistics/statistics.html',
+    controller: 'StatisticsCtrl'
+  });
+}])
+
+.controller('StatisticsCtrl', function($scope, $rootScope, $cookieStore) {
+	$rootScope.loggedIn = $cookieStore.get('loggedIn');
+	if (!$rootScope.loggedIn) {
+		window.location.href = '#!/login';
+	}
+
+  });
