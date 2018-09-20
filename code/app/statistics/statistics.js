f8cce98318f49f187035987b04e78ba12c205dbe
Harsh Parakh <hparakh@purdue.edu>
2018-09-20 19:30:57 -0400

    Fetching total users, share, unique companies and ranking list on backend for statistics page

--- code/app/statistics/statistics.js
+++ code/app/statistics/statistics.js
@@ -14,7 +14,9 @@ angular.module('myApp.statistics', ['ngRoute', 'ngCookies']) //'ngSanitize'
 	if (!$rootScope.loggedIn) {
 		window.location.href = '#!/login';
 	}
-var numberOfUsers = 0;
+var totalUsers = 0;
+var totalShares = 0;
+var totalUniqueComp = 0;
 var map = new Map();
 //Number of users in StockMock
 
@@ -22,7 +24,7 @@ var query = firebase.database().ref("/user/").orderByKey();
 query.once("value")
   .then(function(snapshot) {
     snapshot.forEach(function() {
-    numberOfUsers++;
+    totalUsers++;
   });
 });
 
@@ -36,6 +38,7 @@ query.once("value").then(function(snapshot) {
          for(var key in stocks) {
            var symbol = String(key).toUpperCase();
            var shares = parseInt(stocks[key],10);
+           totalShares += shares;
            if(!map.has(symbol)){
                map.set(symbol, shares);
            }
@@ -44,15 +47,17 @@ query.once("value").then(function(snapshot) {
            }
          }
       }
-
     })
 }).then(() => {
-//  console.log(map);
+totalUniqueComp = map.size;
 const mapSort = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
-//console.log(mapSort1);
+
 for (let [key, value] of mapSort) {     // Print sorted data
      console.log(key + ' ' + value);
  }
+ console.log("Users on StockMock", totalUsers);
+ console.log("Shares traded on StockMock", totalShares);
+ console.log("Unique Companies on StockMock", totalUniqueComp);
 })
 
 
