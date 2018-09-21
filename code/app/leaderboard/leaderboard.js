af6d418d28b2d75ab9d8d4b1c0f9c497c365ec4e
Harsh Parakh <hparakh@purdue.edu>
2018-09-21 02:05:31 -0400

    Leaderboard list being produced

--- code/app/leaderboard/leaderboard.js
+++ code/app/leaderboard/leaderboard.js
@@ -15,4 +15,27 @@ angular.module('myApp.leaderboard', ['ngRoute', 'ngCookies']) //'ngSanitize'
 		window.location.href = '#!/login';
 	}
 
+//Initialize Global Variables
+var map = new Map();
+
+  //Fetch number of shares purchased for each company on StockMock
+  var query = firebase.database().ref("user");
+  query.once("value").then(function(snapshot) {
+      snapshot.forEach(function(user) {
+        if(user.val().email !== undefined && user.val().balance !== undefined){
+          var username = String(user.val().email);
+          var portfolioValue = parseFloat(user.val().balance);
+          if(!map.has(username))
+            map.set(username, portfolioValue);
+        }
+      })
+  }).then(() => {
+
+   const mapSort = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
+
+   for (let [key, value] of mapSort) {     // Print leaderboard data
+       console.log(key + ' ' + value);
+   }
+  })
+
   });
