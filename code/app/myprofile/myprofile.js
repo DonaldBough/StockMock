bfe7489e8fd518592d300745048884291b14239f
Rohan Kashuka <rohankashuka@pal-nat186-2-42.itap.purdue.edu>
2018-10-29 18:41:11 -0400

    History added to myprofile

--- code/app/myprofile/myprofile.js
+++ code/app/myprofile/myprofile.js
@@ -110,4 +110,78 @@ angular.module('myApp.myprofile', ['ngRoute', 'ngCookies'])
           callback(null, 0);
         }
       }
+
+
+      $scope.$on('$routeChangeSuccess', function(next, current) {
+		
+			firebaseGetTransactions(function(message, transactions) {
+				if (transactions != null) {
+					$scope.transactions = transactions.reverse();
+					$scope.$apply();
+				}
+			});
+		
+	});
+
+
+    $rootScope.filter = function(value) {
+		$rootScope.buttons = value;
+    } 
+    
+    function Transaction(email, uid, amount, shares, date, type) {
+		// this._local =  Transaction();
+		this.email= email;
+		this.uid = uid;
+		this.amount = amount;
+		this.shares = shares;
+		this.date = date;
+		this.type = type;
+	  }
+
+      var firebaseGetTransactions = function(callback) {
+		var user = firebase.auth().currentUser;
+		if (user != null) {
+		  var path = "/transactions/" + user.uid;
+		  firebase.database().ref(path).once('value').then(function(snapshot) {
+			if (snapshot.exists())
+			var transactions = [];
+			let dictionary = snapshot.toJSON();
+
+			for (let entry in dictionary) {
+			  var transaction = new Transaction(dictionary[entry].email, dictionary[entry].uid, dictionary[entry].amount, dictionary[entry].shares, dictionary[entry].date, dictionary[entry].type);
+			  transactions.push(transaction);
+			}
+			callback("Transactions succesfully retrived.", transactions);
+		  });
+
+		} else {
+		  callback("Could not retrive transaction data beause you arent signed in", []);
+
+		}
+
+	  }
+
+      $scope.showAll = function(){
+		$scope.dividend = true;
+		$scope.trade = true;
+		$scope.transfer = true;
+	}
+
+	$scope.showTrades = function(){
+		$scope.dividend = false;
+		$scope.trade = true;
+		$scope.transfer = false;
+	}
+
+	$scope.showDividends = function(){
+		$scope.dividend = true;
+		$scope.trade = false;
+		$scope.transfer = false;
+	}
+
+	$scope.showTransfers = function(){
+		$scope.dividend = false;
+		$scope.trade = false;
+		$scope.transfer = true;
+	}
 });
