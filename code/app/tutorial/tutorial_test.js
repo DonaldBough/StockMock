fe315278024ab4d676961093d1697dbf898e8986
james702 <james111@purdue.edu>
2018-12-06 19:26:57 -0500

    Final testcases

--- code/app/tutorial/tutorial_test.js
+++ code/app/tutorial/tutorial_test.js
@@ -8,14 +8,20 @@ describe('StockMock Tutorial', function() {
 	browser.sleep(1000);
 	expect(browser.getCurrentUrl()).toContain("profile");
 	expect(element(by.name('greeting')).getText()).toEqual('Hello user5@test.com!');
-		
-	element(by.name('tools')).click();
+
+
 	element(by.name('tutorial')).click();
-	  
+  element(by.name('demo')).click();
+
 	expect(browser.getCurrentUrl()).toContain("tutorial");
-	  
-	element(by.name('account')).click();
-	element(by.name('logout')).click();
-	
+
+
+  });
+
+  it('Should log out', function() {
+    element(by.name('account')).click();
+  	element(by.name('logout')).click();
+
+    expect(browser.getCurrentUrl()).toContain("login");
   });
 });
